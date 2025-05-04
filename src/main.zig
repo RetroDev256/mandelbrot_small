d613a58ac906430c5040a9347807483706617b09
Eric Petersen <ericpetersen3.14@gmail.com>
2025-05-03 20:10:23 -0600

    eh what's the point?

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,12 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
-const Coord = struct { x: i32, y: i32 };
-const Vec = struct { re: f32, im: f32 };
-
 // Options
-const dim: Coord = .{ .x = 3840, .y = 2400 };
-const offset: Vec = .{ .re = -0.5, .im = 0 };
-const max_iter: u32 = 1024;
-const zoom: f32 = 1.0;
+const dim_x: i32 = 1920;
+const dim_y: i32 = 1200;
+const max_iter: u32 = 256;
 
 pub export fn _start() callconv(.c) noreturn {
-
     // Output File
     const flags: u32 = 0b0000_0010_0100_0001;
     const raw_fd = std.os.linux.open("out.ppm", @bitCast(flags), 0o644);
@@ -19,51 +14,44 @@ pub export fn _start() callconv(.c) noreturn {
     defer std.os.linux.close(fd);
 
     // NetPPM Header
-    write(fd, std.fmt.comptimePrint("P6\n{} {}\n255\n", dim));
+    write(fd, std.fmt.comptimePrint("P5\n{} {}\n255\n", .{ dim_x, dim_y }));
+
+    const dim_x_2 = dim_x / 2;
+    const dim_y_2 = dim_y / 2;
+    const norm: f32 = @floatFromInt(@max(dim_x_2, dim_y_2));
 
     // Per-Pixel Render
-    for (0..dim.y) |y| {
-        for (0..dim.x) |x| {
-            const pos: Coord = .{ .x = @intCast(x), .y = @intCast(y) };
-            const iter = mandelbrot(transform(pos));
+    for (0..dim_y) |y| {
+        for (0..dim_x) |x| {
+            const pos_x: i32 = @intCast(x);
+            const pos_y: i32 = @intCast(y);
 
-            var color: [3]u8 = .{ 0, 0, 0 };
+            const cpos_x: f32 = @floatFromInt(pos_x - dim_x_2);
+            const cpos_y: f32 = @floatFromInt(pos_y - dim_y_2);
+            const scale = 2.0 / norm;
 
-            if (max_iter - iter > 0) {
-                color = @splat(@truncate(iter * 17));
+            const c_re = (cpos_x * scale) - 0.5;
+            const c_im = cpos_y * scale;
+
+            var z_re: f32 = 0;
+            var z_im: f32 = 0;
+
+            var color: u8 = 0xFF;
+
+            for (0..max_iter) |_| {
+                const z_re_2 = z_re * z_re;
+                const z_im_2 = z_im * z_im;
+                if (z_re_2 + z_im_2 > 8) color = 0;
+                const temp = z_re_2 - z_im_2 + c_re;
+                z_im = 2 * z_re * z_im + c_im;
+                z_re = temp;
             }
 
-            write(fd, &color);
+            write(fd, &.{color});
         }
     }
 
     std.os.linux.exit(0);
-}
-
-fn transform(pos: Coord) Vec {
-    const centered_pos_x: f32 = @floatFromInt(pos.x - dim.x / 2);
-    const centered_pos_y: f32 = @floatFromInt(pos.y - dim.y / 2);
-    const normalizer: f32 = @floatFromInt(@max(dim.x / 2, dim.y / 2));
-    const scale = (zoom * 2) / normalizer;
-    return .{
-        .re = (centered_pos_x * scale) + offset.re,
-        .im = (centered_pos_y * scale) + offset.im,
-    };
-}
-
-fn mandelbrot(c: Vec) u32 {
-    var z: Vec = c;
-    for (0..max_iter) |iter| {
-        const z_re_2 = z.re * z.re;
-        const z_im_2 = z.im * z.im;
-
-        if (z_re_2 + z_im_2 > 8) return iter;
-
-        const temp = z_re_2 - z_im_2 + c.re;
-        z.im = 2 * z.re * z.im + c.im;
-        z.re = temp;
-    }
-    return max_iter;
 }
 
 fn write(fd: i32, bytes: []const u8) void {
